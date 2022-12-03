0c58926be4f78e3b501a6d8801ec33078b30cb3d
Vittorio Robecchi <vittorio.robecchi@gmail.com>
2022-12-03 19:02:23 +0100

    upload

---
+++ Day_2/2_part.c
@@ -0,0 +1,18 @@
+// x = 1, Y = 2, Z = 3
+// vinta = +6
+// pari = lettera + lettera
+// tot = somma di tutti i precedenti
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+int main(){
+    FILE *f;
+    f = fopen("input.txt", "r");
+
+    
+
+    fclose(f);
+}
