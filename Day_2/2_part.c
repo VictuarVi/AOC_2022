850c24bd15d80575933c0bc7c85c59f9bbe7e30c
Vittorio Robecchi <10851734@polimi.it>
2022-12-03 20:01:47 +0100

    Maybe funziona, ma so mia tant sicur

--- Day_2/2_part.c
+++ Day_2/2_part.c
@@ -5,14 +5,43 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <time.h>
+#include <string.h>
 
-int main(){
+int main()
+{
     FILE *f;
     f = fopen("input.txt", "r");
+    char p[4];
+    int s = 0, st = 0;
 
-    
+    int n = 0;
+    while (!feof(f))
+    {   
+        fgets(p, 4, f);
+        if (0 == strcmp(p, "A X"))
+            s += 2;
+        if (0 == strcmp(p, "A Y"))
+            s += 8;
+        if (0 == strcmp(p, "A Z"))
+            s += 3;
+        if (0 == strcmp(p, "B X"))
+            s += 1;
+        if (0 == strcmp(p, "B Y"))
+            s += 4;
+        if (0 == strcmp(p, "B Z"))
+            s += 9;
+        if (0 == strcmp(p, "C X"))
+            s += 7;
+        if (0 == strcmp(p, "C Y"))
+            s += 2;
+        if (0 == strcmp(p, "C Z"))
+            s += 6;
+        
+        st = st + s;
+        s = 0;
+    }
+
+    printf ("\nst = %d", st);
 
     fclose(f);
 }
