a8fd134de356a338d1d8136a40812d95c4fc8874
Vittorio Robecchi <10851734@polimi.it>
2022-12-04 18:23:15 +0100

    Sono un clown☺

--- Day_3/part1.c
+++ Day_3/part1.c
@@ -7,9 +7,9 @@ int main(int argc, char *argv[])
 {
     FILE *f;
     f = fopen(argv[1], "r");
-    char b[70], p1[35], p2[35], p;  // trasformato p in char, non più array[1] di char
-    int l;     // lunghezza della stringa
-    int s = 0; // somma delle priorità
+    char b[70], p1[35], p2[35], p; // trasformato p in char, non più array[1] di char
+    int l;                         // lunghezza della stringa
+    int s = 0;                     // somma delle priorità
 
     while (!feof(f))
     {
@@ -20,26 +20,14 @@ int main(int argc, char *argv[])
         //      printf("%c\n", ch);
 
         l = strlen(b);
-        // La prima stringha a quanto pare la leggeva da 19
-        if (l % 2 == 1) l--;
-        // printf ("l = %d", l);
+        if (l % 2 == 1)
+            l--;
 
         for (int i = 0; i < l / 2; i++)
         {
             p1[i] = b[i];
             p2[i] = b[i + l / 2];
         }
-
-        // printf ("\nCiclo %d | ", ++n);
-        // for (int i = 0; i < l / 2; i++)
-        // {
-        //     printf ("%c", p1[i]);
-        // }
-        // printf ("----");
-        // for (int i = 0; i < l / 2; i++)
-        // {
-        //     printf ("%c", p2[i]);
-        // }
 
         bool flag = true;
         for (int j = 0; j < l / 2 && flag; j++)
@@ -55,18 +43,6 @@ int main(int argc, char *argv[])
         }
 
         int v;
-        // printf (" | p = %c", p[0]);
-        // if (p[0] >= 'a' && p[0] <= 'z'){
-        //     v = p[0]-96;
-        //     printf (" => %d", v);
-        //     s = s + v;
-        // }
-        // else {
-        //     v = tolower(p[0]);
-        //     v = p[0]-38;
-        //     printf (" => %d", v);
-        //     s = s + v;
-        // }
 
         if (p >= 'a' && p <= 'z')
         {
