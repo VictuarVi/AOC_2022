e09c559e8f7a5b1b881f2033cb20ac6d657375a7
Vittorio Robecchi <vittorio.robecchi@gmail.com>
2022-12-10 13:34:19 +0100

    Top con le librerie

--- Day_10/Day10_Part1.c
+++ Day_10/Day10_Part1.c
@@ -1,25 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-FILE *file_control(int argv, char *argc[])
-{
-    if (argv != 2)
-    {
-        printf("Only 2 parameters arguments allowed.");
-        exit(-1);
-    }
-
-    FILE *fp;
-    fp = fopen(argc[1], "r");
-    if (fp == NULL)
-    {
-        printf("The file can't be read.");
-        exit(-1);
-    }
-
-    return fp;
-}
+#include "../Libraries/file.h"
 
 int main(int argv, char *argc[])
 {
